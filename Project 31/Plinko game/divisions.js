f91a1e6baea9b957e20a1e64566ff24ce8c104ae
prachiArora05 <80327613+prachiArora05@users.noreply.github.com>
2021-11-21 17:32:25 +0530

    plinko game by prachi

---
+++ Project 31/Plinko game/divisions.js
@@ -0,0 +1,14 @@
+class Divisions {
+  constructor(x, y, w, h) {
+    this.body = Bodies.rectangle(x, y, w, h, {isStatic: true});
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+  }
+  display() {
+    var pos = this.body.position;
+    rectMode(CENTER);
+    fill("white");
+    rect(pos.x, pos.y, this.w, this.h);
+  }
+}
